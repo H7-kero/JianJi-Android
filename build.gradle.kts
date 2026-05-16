7c6d1979a36c99e5cfee8b25cdd5abbc8f591697
H7-kero <heng940715@gmail.com>
2026-05-16 21:48:07 +0800

    fix: 更新 KSP 版本到 2.0.21-1.0.25

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,5 +12,5 @@ plugins {
     id("com.android.application") version "9.2.1" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
-    id("com.google.devtools.ksp") version "2.0.20-1.0.22" apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.25" apply false
 }
