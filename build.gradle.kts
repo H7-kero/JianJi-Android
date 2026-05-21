7ac37d4f136244f9766d152561641296e456eb7d
H7-kero <heng940715@gmail.com>
2026-05-22 02:32:02 +0800

    fix: correct KSP version to 2.2.0-2.0.2 for Kotlin 2.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,5 +12,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.2.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
-    id("com.google.devtools.ksp") version "2.2.0-1.0.21" apply false
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
 }
